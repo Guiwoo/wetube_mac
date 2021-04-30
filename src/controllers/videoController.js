6d27b08609990dd6271ad969f651ce2f29a3ed4d
Guiwoo Park <guiwoopark@Guiwooui-MacBookAir.local>
2021-04-30 15:49:30 +0900

    templating

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,34 @@
-export const trending = (req,res) => res.render("home",{pageTitle: 'Home'})
+const fakeUser = {
+    username : "guiwoo",
+    loggedin : false
+} 
+
+export const trending = (req,res) => {
+    const videos = [{
+        title:"Hello",
+        rating:5,
+        comments:2,
+        createdAt:"2 mins ago",
+        view:59,
+        id:1,
+    },{
+        title:"Second",
+        rating:8,
+        comments:32,
+        createdAt:"5 mins ago",
+        view:30000,
+        id:2,
+    },{
+        title:"Third",
+        rating:10,
+        comments:100,
+        createdAt:"10 mins ago",
+        view:12392193,
+        id:3,
+    }
+]
+    return res.render("home",{pageTitle: 'Home', fakeUser, videos});
+}
 
 export const search = (req,res)=>res.render("search")
 
