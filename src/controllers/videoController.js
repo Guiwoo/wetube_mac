ad8dd537621a83c019d3431d5555977f33c6d41f
Guiwoo Park <guiwoopark@Guiwooui-MacBookAir.local>
2021-07-09 20:41:29 +0900

    setUp_MongoDb

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,58 +1,86 @@
 const fakeUser = {
-    username : "guiwoo",
-    loggedin : false
-} 
+  username: "guiwoo",
+  loggedin: false,
+};
 
-let videos = [{
-    title:"Hello",
-    rating:5,
-    comments:2,
-    createdAt:"2 mins ago",
-    view:59,
-    id:1,
-},{
-    title:"Second",
-    rating:8,
-    comments:32,
-    createdAt:"5 mins ago",
-    view:30000,
-    id:2,
-},{
-    title:"Third",
-    rating:10,
-    comments:100,
-    createdAt:"10 mins ago",
-    view:12392193,
-    id:3,
-}
-]
+let videos = [
+  {
+    title: "Hello",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 mins ago",
+    view: 59,
+    id: 1,
+  },
+  {
+    title: "Second",
+    rating: 8,
+    comments: 32,
+    createdAt: "5 mins ago",
+    view: 30000,
+    id: 2,
+  },
+  {
+    title: "Third",
+    rating: 10,
+    comments: 100,
+    createdAt: "10 mins ago",
+    view: 12392193,
+    id: 3,
+  },
+];
 
-export const trending = (req,res) => {
-    return res.render("home",{pageTitle: 'Home', fakeUser, videos});
-}
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
 
-export const search = (req,res)=>res.render("search")
+export const search = (req, res) => res.render("search");
 
+export const watch = (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watch ${video.title}`, fakeUser, video });
+};
 
-export const watch = (req,res)=> {
-    const {params:{id}}=req
-    const video = videos[id-1]
-    res.render("watch",{pageTitle:`Watch ${video.title}`, fakeUser,video});
-}
+export const getHandleEdit = (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing ${video.title}`, fakeUser, video });
+};
 
-export const getHandleEdit = (req,res) => {
-   const {params:{id}}=req
-   const video = (videos[id-1])
-   res.render("edit",{pageTitle:`Editing ${video.title}`,fakeUser,video})
-}
+export const postHandleEdit = (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const {
+    body: { title },
+  } = req;
+  videos[id - 1].title = title;
+  res.redirect(`/videos/${id}`);
+};
 
-export const postHandleEdit = (req,res) => {
-    const {params:{id}} = req
-    const {body:{title}} =req
-    videos[id-1].title = title
-    res.redirect(`/videos/${id}`)
-}
+export const deleteVideo = (req, res) => res.send("Delete Video");
 
-export const deleteVideo = (req,res)=>res.send("Delete Video")
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video", fakeUser });
+};
 
-export const uploadVideo = (req,res)=>res.send("Upload Video")
+export const postUpload = (req, res) => {
+  //will add a video to the videos array
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "1sec ago",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,18 +1,25 @@
 import express from "express";
-import { deleteVideo, getHandleEdit, postHandleEdit, uploadVideo, watch } from "../controllers/videoController";
+import {
+  deleteVideo,
+  getHandleEdit,
+  getUpload,
+  postHandleEdit,
+  postUpload,
+  watch,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 //http://expressjs.com/en/guide/routing.html check up this site about option group
 //https://www.regexpal.com Result of regex
 
+videoRouter.get("/:id(\\d+)", watch);
 
-videoRouter.get("/:id(\\d+)",watch)
+videoRouter.route("/:id(\\d+)/edit").get(getHandleEdit).post(postHandleEdit);
 
-videoRouter.route("/:id(\\d+)/edit").get(getHandleEdit).post(postHandleEdit)
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
-videoRouter.get("/:id(\\d+)/delete",deleteVideo)
-
-videoRouter.get("/upload",uploadVideo)
+// videoRouter.get("/upload",uploadVideo)
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
